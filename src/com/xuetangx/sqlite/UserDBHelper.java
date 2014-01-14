7f7afa9e717681873df972898e5cab57e9894690
gaoyan10 <gaoyansansheng@gmail.com>
2014-01-14 12:51:50 +0800

    sqlite create table for current_user

--- src/com/xuetangx/sqlite/UserDBHelper.java
+++ src/com/xuetangx/sqlite/UserDBHelper.java
@@ -1,5 +1,29 @@
 package com.xuetangx.sqlite;
 
-public class UserDBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.xuetangx.util.ConstantUtils;
+
+public class UserDBHelper extends SQLiteOpenHelper {
+	private Context context;
+	private static final int VERSION = 1;
+	public UserDBHelper(Context c) {
+		super(c, ConstantUtils.USER_DATA, null, VERSION);
+	}
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		String sql = "create table if not exists current_user(username TEXT, access_token TEXT, scope TEXT, expires_in INTEGER, refresh_token TEXT)";
+		db.execSQL(sql);
+	}
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		if(newVersion > oldVersion) {
+			// update db, add field.
+		}
+	}
 
 }
--- src/com/xuetangx/sqlite/UserDBManager.java
+++ src/com/xuetangx/sqlite/UserDBManager.java
@@ -1,5 +1,90 @@
 package com.xuetangx.sqlite;
 
+import java.util.HashMap;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
 public class UserDBManager {
+	private UserDBHelper helper;
+	private SQLiteDatabase db;
+	private static UserDBManager instance;
+	public static synchronized UserDBManager getUserDBManager(Context context) {
+		if(instance == null) {
+			instance = new UserDBManager(context);
+		}
+		return instance;
+	}
+	private UserDBManager(Context c) {
+		helper = new UserDBHelper(c);
+		db = helper.getWritableDatabase();
+	}
+	/**
+	 * keep the table has only one row.
+	 * @param access
+	 * @return
+	 */
+	public boolean insertAccess(String[] access) {
+		db.beginTransaction();
+		try {
+			if(access.length < 5) {
+				throw new Exception();
+			}
+			db.execSQL("delete from current_user");
+			String sql = "insert into current_user values(?,?,?,?,?)";
+			db.execSQL(sql, access);
+			return true;
+		}catch(Exception e) {
+			return false;
+		} finally {
+			db.endTransaction();
+		}
+	}
+	/**
+	 * get all message.
+	 * @return all message.
+	 */
+	public HashMap<String, Object> getUserMessage() {
+		HashMap<String, Object> message = new HashMap<String, Object>();
+		String sql = "select * from current_user";
+		Cursor c = db.rawQuery(sql, null);
+		if (c.getCount() > 1) {
+			return null;
+		}
+		try {
+			while (c.moveToNext()) {
+				message.put("username", c.getString(c.getColumnIndex("username")));
+				message.put("access_token", c.getString(c.getColumnIndex("access_token")));
+				message.put("refresh_token", c.getString(c.getColumnIndex("refresh_token")));
+				message.put("scope", c.getString(c.getColumnIndex("scope")));
+				message.put("expires_in", c.getInt(c.getColumnIndex("expires_in")));
+			}
+		}catch(Exception e) {
+			return null;
+		}finally {
+			c.close();
+		}
+		return message;
+	}
+	public String getStringField(String field) {
+		HashMap<String, Object> message = new HashMap<String, Object>();
+		String sql = "select * from current_user";
+		Cursor c = db.rawQuery(sql, null);
+		if (c.getCount() > 1) {
+			return null;
+		}
+		try {
+			if (c.moveToNext()) {
+				return c.getString(c.getColumnIndex(field));
+			}
+		}catch(Exception e) {
+			return null;
+		}finally {
+			c.close();
+		}
+		return null;
+	}
+	public String get
 
 }
